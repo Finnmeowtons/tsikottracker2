9d33db0e0b9a1eb24779787f2cee7e81bacaaae2
CJ Soriano <soriano.christianjose.m@gmail.com>
2024-02-24 22:14:06 +0800

    debug

--- app/Http/Controllers/OffersApi.php
+++ app/Http/Controllers/OffersApi.php
@@ -27,7 +27,7 @@ class OffersApi extends Controller
 
         $offer = Offer::create($validatedData);
         Log::debug($request->all());
-        return response('Hello World', 201); // 201 Created status
+        return response()->json($offer, 201); // 201 Created status
     }
 
 
