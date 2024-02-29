cfe4e49b128da1f96e497d8206d73fbd2b8ef940
CJ Soriano <soriano.christianjose.m@gmail.com>
2024-02-29 17:14:24 +0800

    debug

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -28,15 +29,17 @@ class UserController extends Controller
     public function getUserCompanies(Request $request, $userId): JsonResponse
  {
      // Validate user ID (optional, but recommended)
-     if (!is_numeric($userId)) {  
-         return response()->json([
-             'error' => 'Invalid user ID'
-         ], 400); // Bad Request
-     }
+    if (!is_numeric($userId)) {  
+        return response()->json([
+            'error' => 'Invalid user ID'
+        ], 400); // Bad Request
+    }
 
-     $user = User::find($userId);  
-
-     $companies = $user->companies()->with(['offer', 'customers'])->get();  
+    // Find companies where user_id exists in UserCompanyRelationship
+    $companies = Company::join('user_company_relationships', 'user_company_relationships.company_id', '=', 'companies.id')
+                ->where('user_company_relationships.user_id', $userId)
+                ->with(['offer', 'customers'])
+                ->get();
 
     return response()->json([
         'companies' => $companies
