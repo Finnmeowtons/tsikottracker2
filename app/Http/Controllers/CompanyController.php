72156bba516eca6e04e057520e4f95f4776c1611
CJ Soriano <soriano.christianjose.m@gmail.com>
2024-03-03 01:02:46 +0800

    debug

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -20,7 +20,7 @@ class CompanyController extends Controller
     }
 
     public function getOwnCompany(Request $request, $id){
-        $company = Company::where('owner_id', $id);
+        $company = Company::where('id', $id);
 
         if ($company) {
             return response()->json($company); 
