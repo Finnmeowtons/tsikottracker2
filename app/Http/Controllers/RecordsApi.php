b9ae360cefe4333e6e6fdc36efa21dd40913892f
CJ Soriano <soriano.christianjose.m@gmail.com>
2024-03-05 00:12:26 +0800

    debug

--- app/Http/Controllers/RecordsApi.php
+++ app/Http/Controllers/RecordsApi.php
@@ -7,6 +7,7 @@ use App\Models\Employee;
 use App\Models\Offer;
 use App\Models\Record;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class RecordsApi extends Controller
 {
@@ -17,67 +18,73 @@ class RecordsApi extends Controller
     }
 
     public function getOwnRecord(Request $request, $id)
-{
-    $records = Record::where('company_id', $id)
-        ->with(['customer', 'offer', 'employee'])
-        ->get();
+    {
+        $records = Record::where('company_id', $id)
+            ->with(['customer', 'offer', 'employee'])
+            ->get();
 
-    $formattedRecords = $records->map(function ($record) {
-        return [
-            'customer_name' => $record->customer->name,
-            'customer_car_plate_number' => $record->customer->car_plate_number,
-            'offer_id' => $record->offer->id,
-            'offer' => $record->offer->name,
-            'offer_price' => $record->offer->price,
-            'type' => $record->offer->type,
-            'date' => $record->date,
-            'company_id' => $record->company_id,
-            'notes' => $record->notes,
-            'employee_id' => $record->employee_id,
-            'employee_name' => $record->employee->name,
-            'employee_position' => $record->employee->position,
-            'id' => $record->id,
-            'time' => $record->created_at
-        ];
-    });
+        $formattedRecords = $records->map(function ($record) {
+            return [
+                'customer_name' => $record->customer->name,
+                'customer_car_plate_number' => $record->customer->car_plate_number,
+                'offer_id' => $record->offer->id,
+                'offer' => $record->offer->name,
+                'offer_price' => $record->offer->price,
+                'type' => $record->offer->type,
+                'date' => $record->date,
+                'company_id' => $record->company_id,
+                'notes' => $record->notes,
+                'employee_id' => $record->employee_id,
+                'employee_name' => $record->employee->name,
+                'employee_position' => $record->employee->position,
+                'id' => $record->id,
+                'time' => $record->created_at
+            ];
+        });
 
-    return response()->json($formattedRecords);
-}
+        return response()->json($formattedRecords);
+    }
 
     public function storeRetrofit(Request $request)
     {
-        $validatedData = $request->validate([
-            'customer_name' => 'nullable',
-            'car_plate_number' => 'required',
-            'offer' => 'required|max:255',
-            'offer_price' => 'required',
-            'type' => 'required',
-            'notes' => 'nullable',
-            'employee_name' => 'nullable|max:255',
-            'employee_position' => 'nullable|max:255',
-            'company_id' => 'required|exists:companies,id'
-        ]);
+        try {
+            $validatedData = $request->validate([
+                'customer_name' => 'nullable',
+                'customer_car_plate_number' => 'required',
+                'offer' => 'required|max:255',
+                'offer_price' => 'required',
+                'type' => 'required',
+                'notes' => 'nullable',
+                'employee_name' => 'nullable|max:255',
+                'employee_position' => 'nullable|max:255',
+                'company_id' => 'required'
+            ]);
 
-        $customer = Customer::firstOrCreate(['name' => $request->customer_name]);
-        $offer = Offer::firstOrCreate(['name' => $request->offer]); 
-        $employee = Employee::firstOrCreate(['name' => $request->employee_name]);
+            $customer = Customer::firstOrCreate(['name' => $request->customer_name]);
+            $offer = Offer::firstOrCreate(['name' => $request->offer]);
+            $employee = Employee::firstOrCreate(['name' => $request->employee_name]);
 
-        $validatedData['customer_id'] = $customer->id;
-        $validatedData['service_product_id'] = $offer->id;
-        $validatedData['employee_id'] = $employee->id;
+            $validatedData['customer_id'] = $customer->id;
+            $validatedData['service_product_id'] = $offer->id;
+            $validatedData['employee_id'] = $employee->id;
 
-        $record = Record::create($validatedData);
+            $record = Record::create($validatedData);
 
-        return response()->json($record);
+            return response()->json($record, 201);
 
+        } catch (ValidationException $e) {
+            return response()->json([
+                'errors' => $e->validator->getMessageBag() // Nice error format 
+            ], 422); // Status code for Unprocessable Entity 
+        }
     }
 
     public function store(Request $request)
     {
         $validatedData = $request->validate([
             'customer_id' => 'nullable|exists:customers,id',
-            'service_product_id' => 'required|exists:offers,id', 
-            'price' => 'nullable|numeric|min:0', 
+            'service_product_id' => 'required|exists:offers,id',
+            'price' => 'nullable|numeric|min:0',
             'date' => 'date_format:Y-m-d h:i:s|nullable',
             'company_id' => 'required|exists:companies,id',
             'notes' => 'nullable',
@@ -93,7 +100,7 @@ class RecordsApi extends Controller
 
     public function show(Record $record)
     {
-        return response()->json($record); 
+        return response()->json($record);
     }
 
     public function update(Request $request, Record $record)
@@ -108,8 +115,8 @@ class RecordsApi extends Controller
             'employee_id' => 'nullable|exists:employees,id',
         ]);
 
-        $record->update($validatedData); 
-        return response()->json($record);    
+        $record->update($validatedData);
+        return response()->json($record);
 
     }
 
