786aad232890881f1e4c19c3273485ebdd2b07fa
CJ Soriano <soriano.christianjose.m@gmail.com>
2024-03-09 04:54:07 +0800

    ww

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -4,12 +4,23 @@ namespace App\Http\Controllers;
 use App\Exports\RecordsExport;
 use App\Models\Record;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Request;
 use Maatwebsite\Excel\Facades\Excel;
 
 class RequestController extends Controller
 {
-    public function export() 
+    public function export(Request $request) 
     {
+        $companyName = $request->get('companyName'); 
+        $contactName = $request->get('contactName');
+
+        // Filter records based on companyName and contactName
+        $records = Record::with(['offers', 'employee'])
+                         ->whereHas('customer', function($query) use ($companyName, $contactName) {
+                             $query->where('company_name', 'like', '%' . $companyName . '%') 
+                                   ->where('customer_name', 'like', '%' . $contactName . '%');
+                         })
+                         ->get(); 
         return Excel::download(new RecordsExport, 'customer_data.xlsx');
     }
 }
