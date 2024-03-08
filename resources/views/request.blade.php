24aa226de38d0cb91861a4316de8e02525debc76
CJ Soriano <soriano.christianjose.m@gmail.com>
2024-03-08 21:34:08 +0800

    web page

--- resources/views/request.blade.php
+++ resources/views/request.blade.php
@@ -1,11 +1,204 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Tsikot Tracker</title>
 </head>
+
 <body>
-    
+    <!-- Header -->
+    <section id="header">
+        <div class="header container">
+            <div class="nav-bar">
+                <div class="brand">
+                    <a href="#hero">
+                        <h1><span>Tsikot</span> Tracker</h1>
+                    </a>
+                </div>
+                <div class="nav-list">
+                    <ul>
+                        <li><a href="#hero" data-after="Home">Tsikot Tracker</a></li>
+                        <li><a href="#services" data-after="Service">About Us</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Header -->
+    <!-- Hero Section -->
+    <section id="hero">
+        <div class="hero container">
+            
+            <div>
+                <h1>Tsikot Tracker, <span></span></h1>
+                <h1>Your Automotive Service <span></span></h1>
+                <h1>Management System <span></span></h1>
+                <a href="#projects" type="button" class="cta">Request Data</a>
+            </div>
+        </div>
+    </section>
+    <!-- End Hero Section -->
+    <!-- Service Section -->
+    <section id="services">
+        <div class="services container">
+            <div class="service-top">
+                <h1 class="section-title">Wha<span>t</span><span> i</span>s Tsikot<span> T</span>rack<span>e</span>r<span>?</span></h1>
+                <p>Tsikot Tracker isn't just a tool for car service businesses; it's a bridge connecting customers with seamless data access. In addition to empowering businesses with streamlined operations, Tsikot Tracker offers customers the ability to request their data from the company effortlessly. Whether it's retrieving service history or accessing vehicle details, our secure website and Android app ensure customers can conveniently access their information, fostering transparency and trust between businesses and their clientele. With Tsikot Tracker, it's not just about managing operations; it's about empowering both businesses and customers alike for a smoother, more connected automotive service experience.</p>
+            </div>
+            <div class="service-bottom">
+                <div class="service-item">
+                    <div class="icon"><img src="./img/data-analytics.png" /></div>
+                    <h2>Analytics</h2>
+                    <p>Detailed performance insights for strategic planning.
+                    </p>
+                </div>
+                <div class="service-item">
+                    <div class="icon"><img src="./img/request.png" /></div>
+                    <h2>Data Request</h2>
+                    <p>Seamlessly access and manage service history.</p>
+                </div>
+                <div class="service-item">
+                    <div class="icon"><img src="./img/data.png" /></div>
+                    <h2>Record Input</h2>
+                    <p>Simplified management of business records</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Service Section -->
+    <!-- Projects Section -->
+    <section id="projects">
+        <div class="projects container">
+            <div class="projects-header">
+                <h1 class="section-title">Recent <span>Projects</span></h1>
+            </div>
+            <div class="all-projects">
+                <div class="project-item">
+                    <div class="project-info">
+                        <h1>Community Outreach and Education Program</h1>
+                        <p>Our Community Outreach Program educates local pet owners through workshops and seminars on responsible pet care, preventive healthcare, and spaying/neutering, promoting informed decision-making for healthier pets and happier communities.</p>
+                    </div>
+                    <div class="project-img">
+                        <img src="./img/img-3.jpg" alt="img">
+                    </div>
+                </div>
+                <div class="project-item">
+                    <div class="project-info">
+                        <h1>Feral Cat Trap-Neuter-Return (TNR) Program</h1>
+                        <p>With our Feral Cat TNR Program, we've successfully managed the feral cat population by implementing a humane approach—trapping, neutering, and returning them to their colonies. This initiative not only controls overpopulation but also promotes a compassionate community response to the welfare of feral cats.</p>
+                    </div>
+                    <div class="project-img">
+                        <img src="./img/img-2.jpg" alt="img">
+                    </div>
+                </div>
+                <div class="project-item">
+                    <div class="project-info">
+                        <h1>Mobile Veterinary Clinic for Underserved Areas</h1>
+                        <p>Our Mobile Veterinary Clinic overcomes geographical barriers to provide essential vaccinations, health check-ups, and preventive care, ensuring optimal health for pets in remote areas.</p>
+                    </div>
+                    <div class="project-img">
+                        <img src="./img/img-4.jpg" alt="img">
+                    </div>
+                </div>
+                <div class="project-item">
+                    <div class="project-info">
+                        <h1>Pet Microchipping and Identification Drive</h1>
+                        <p>Through our Pet Microchipping Drive, we've enhanced pet safety and reunification efforts by organizing events that offer affordable microchipping services. This initiative has contributed to creating a more secure and responsible pet ownership environment in our community.</p>
+                    </div>
+                    <div class="project-img">
+                        <img src="./img/img-5.jpg" alt="img">
+                    </div>
+                </div>
+                <div class="project-item">
+                    <div class="project-info">
+                        <h1>Veterinary Student Mentorship Program</h1>
+                        <p>The Veterinary Student Mentorship Program supports aspiring veterinarians, facilitating a smooth transition to professionals by connecting students with experienced mentors and enriching the field of veterinary medicine.</p>
+                    </div>
+                    <div class="project-img">
+                        <img src="./img/img-6.jpg" alt="img">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Projects Section -->
+    <!-- About Section -->
+    <section id="about">
+        <div class="about container">
+            <div class="col-left">
+                <div class="about-img">
+                    <img src="./img/img-7.jpg" alt="img">
+                </div>
+            </div>
+            <div class="col-right">
+                <h1 class="section-title">About <span>me</span></h1>
+                <h2>Veterinarian</h2>
+                <p>In the heart of underserved areas, CJ's "Mobile Veterinary Clinic" becomes a beacon of hope, providing essential care and forging deep connections with beloved pets in remote communities. Through the "Feral Cat Trap-Neuter-Return (TNR) Program," CJ transforms the plight of feral cats into a story of compassion, offering them a chance at a better life. In each healing touch and act of kindness, CJ leaves an indelible mark, not just on the animals but on the hearts of those in need, creating a tapestry of warmth and care that defines a truly heartfelt mission.</p>
+                <a href="#" class="cta">Download Resume</a>
+            </div>
+        </div>
+    </section>
+    <!-- End About Section -->
+    <!-- Contact Section -->
+    <section id="contact">
+        <div class="contact container">
+            <div>
+                <h1 class="section-title">Contact <span>info</span></h1>
+            </div>
+            <div class="contact-items">
+                <div class="contact-item">
+                    <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/phone.png" /></div>
+                    <div class="contact-info">
+                        <h1>Phone</h1>
+                        <h2>+63 927 073 4452</h2>
+                        <h2>+63 968 654 2127</h2>
+                    </div>
+                </div>
+                <div class="contact-item">
+                    <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/new-post.png" /></div>
+                    <div class="contact-info">
+                        <h1>Email</h1>
+                        <h2>soriano.christianjose.m@gmail.com</h2>
+                        <h2>hrma.soriano.up@phinmaed.com</h2>
+                    </div>
+                </div>
+                <div class="contact-item">
+                    <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/map-marker.png" /></div>
+                    <div class="contact-info">
+                        <h1>Address</h1>
+                        <h2>Lucao De Venecia, New York City, New Zealand</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Contact Section -->
+    <!-- Footer -->
+    <section id="footer">
+        <div class="footer container">
+            <div class="brand">
+                <h1><span>C</span>hristian <span>J</span>ose</h1>
+            </div>
+            <h2>Your Veterinary Solution</h2>
+            <div class="social-icon">
+                <div class="social-item">
+                    <a href="https://www.facebook.com/ban75474"><img src="https://img.icons8.com/bubbles/100/000000/facebook-new.png" /></a>
+                </div>
+                <div class="social-item">
+                    <a href="https://www.instagram.com/finnmeowmeows/"><img src="https://img.icons8.com/bubbles/100/000000/instagram-new.png" /></a>
+                </div>
+                <div class="social-item">
+                    <a href="https://twitter.com/Johnlennonnn69"><img src="https://img.icons8.com/bubbles/100/000000/x.png" /></a>
+                </div>
+            </div>
+            <p>Copyright © 2020 CJ. All rights reserved</p>
+        </div>
+    </section>
+    <!-- End Footer -->
+    <script src="./app.js"></script>
 </body>
+
 </html>
