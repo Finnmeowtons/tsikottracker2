fd331fdf74655b0ae375b9ed3be3fc33e9f2932e
CJ Soriano <soriano.christianjose.m@gmail.com>
2024-03-15 19:16:28 +0800

    email

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Maatwebsite\Excel\Facades\Excel;
 
 class EmailController extends Controller
 {
-    //
+    public function sendExcelReport(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email' 
+        ]);
+
+        $data = User::all(); // Get data from your database
+
+        Excel::create('database_export', function ($excel) use ($data) {
+            $excel->sheet('Sheet 1', function ($sheet) use ($data) {
+                $sheet->fromArray($data);
+            });
+        })->store('xlsx', public_path('exports'));
+
+        $filePath = public_path('exports/database_export.xlsx');
+        $recipient = $request->input('email');
+
+        Mail::send([], [], function($message) use ($filePath, $recipient) {
+            $message->to($recipient)
+                    ->subject('Database Export')
+                    ->setBody('Hello, Please find the attached database export. Regards, Your Application Name') // Plain text message body
+                    ->attach($filePath); 
+        });
+    }
 }
