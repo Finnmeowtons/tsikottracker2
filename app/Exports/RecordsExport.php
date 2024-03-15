afba7e58dc72e7fc9efd4d2c0e8a9787098a8b13
CJ Soriano <soriano.christianjose.m@gmail.com>
2024-03-16 01:54:54 +0800

    www

--- app/Exports/RecordsExport.php
+++ app/Exports/RecordsExport.php
@@ -19,7 +19,7 @@ class RecordsExport implements FromCollection
         $data = []; // Array to hold our formatted data
 
         Record::whereHas('customer', function($query) {
-            $query->where('name', 'like', '%' . $this->customer_name . '%'); 
+            $query->where('name', $this->customer_name); 
         })->cursor()->each(function (Record $record) use (&$data) {
             if ($record->customer_id) {
                 $data[] = [
